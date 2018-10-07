6e63d52114057f08ba2055a7bdc8977d2302bbe3
Jingna Li <jingna13@gmail.com>
2018-10-07 09:24:56 -0700

    Add additional validation for Email or Mobile Number for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,19 +18,20 @@
 class User < ApplicationRecord
   validates :first_name, uniqueness: {scope: [:last_name]}
   validates :first_name, :last_name, presence: true
-  validates :email, :birth_date, :gender, presence: true
+  validates :birth_date, :gender, presence: true
   validates :password_digest, :session_token, presence: true
-  validates :password, minimum: { length: 6, allow_nil: true }
-
-  after_initialize ensure_session_token
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validate :email_or_mobile_number
+  after_initialize :ensure_session_token
   attr_reader :password
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16)
   end
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
+  def self.find_by_credentials(login, password)
+    user = User.find_by_email(email)
+        || User.find_by_mobile_number( sanitize_mobile(login) )
     return user && user.is_password?(password)
   end
 
@@ -51,6 +52,18 @@ class User < ApplicationRecord
     self.session_token = User.generate_session_token
     self.save!
     self.session_token
+  end
+
+  private
+
+  def email_or_mobile_number
+    if email.nil? && mobile_number.nil?
+      errors[:base] << "Email or Mobile Number Required"
+    end
+  end
+
+  def sanitize_mobile(mobile_number)
+    mobile_number.delete('^0-9')
   end
 
 end
