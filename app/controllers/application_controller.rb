ac6aedc6f3d5c91dde658f9272e0df7c0f4ef8b6
Jingna Li <jingna13@gmail.com>
2018-10-07 09:26:55 -0700

    Add User and Session Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
+  skip_before_action :verify_authenticity_token
 
   def login!(user)
     session[:session_token] = user.reset_session_token!
