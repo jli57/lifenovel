76ea24797040ea915e48a3d20c6f9ae59de28cef
Jingna Li <jingna13@gmail.com>
2018-10-10 11:43:52 -0700

    Add Page and Feed selectors for posts

---
+++ frontend/reducers/selectors.js
@@ -0,0 +1,16 @@
+export const filterPagePosts = ( session, posts ) => (
+  Object.values(posts).filter( post => post.page_id === session.id )
+)
+
+export const filterFeedPosts = ( friends, posts ) => (
+  Object.values(posts).filter( post => friends.includes( post.page_id ) )
+)
+
+export const addAuthorToPosts = ( posts, users ) => (
+  Object.values(posts).map( post => {
+    const author = users[post.author_id];
+    const author_fullname = `${author.first_name} ${author.last_name}`;
+    post.author = author_fullname;
+    return post;
+  })
+)
