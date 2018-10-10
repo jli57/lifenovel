05555e5a71e73b3e124a3b65704c3f11dceba5c1
Jingna Li <jingna13@gmail.com>
2018-10-10 16:41:18 -0700

    Change logout to class component

--- frontend/components/session/logout.jsx
+++ frontend/components/session/logout.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+class LogOut extends React.Component {
 
-const LogOut = () => {
+  constructor(props) {
+    super(props);
+    this.state =
+  }
+  render() {
+    return (
+      <div>
+        <button className="header-btn" onClick={ this.props.logout }>Logout</button>
+      </div>
+    );
+  }
 
-  return (
-    <div>
-      <button className="header-btn" onClick={ logout }>Logout</button>
-    </div>
-  )
 };
 
 export default LogOut;
