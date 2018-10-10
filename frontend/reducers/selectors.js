a622e265affdaf2af6ecdc691301ed8f837cbfc2
Jingna Li <jingna13@gmail.com>
2018-10-10 16:42:10 -0700

    Add selectors

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,9 +1,12 @@
 export const filterPagePosts = ( session, posts ) => (
-  Object.values(posts).filter( post => post.page_id === session.id )
+  Object.values(posts)
+    .filter( post => post.page_id === session.id )
 )
 
 export const filterFeedPosts = ( friends, posts ) => (
-  Object.values(posts).filter( post => friends.includes( post.page_id ) )
+  Object.values(posts)
+    .filter( post => friends.includes( post.page_id ) )
+    .sort( (a, b) => a.created_at - b.created_at )
 )
 
 export const addAuthorToPosts = ( posts, users ) => (
