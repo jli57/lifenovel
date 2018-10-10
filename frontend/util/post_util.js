f8cfdd8e647d5cdb9497862e881080ad07b3edd0
Jingna Li <jingna13@gmail.com>
2018-10-10 10:10:27 -0700

    Add post ajax calls

---
+++ frontend/util/post_util.js
@@ -0,0 +1,28 @@
+export const fetchPosts = (posts) => (
+  $.ajax({
+    method: "GET",
+    url: "/api/posts"
+  })
+)
+
+export const createPost = (post) => (
+  $.ajax({
+    method: "POST",
+    url: `/api/posts`,
+    data: { post }
+  })
+)
+
+export const updatePost = (post) => (
+  $.ajax({
+    method: "PATCH",
+    url: `/api/posts/${post.id}`,
+    data: { post }
+  })
+)
+export const deletePost = () => (
+  $.ajax({
+    method: "DELETE",
+    url: `/api/posts/${post.id}`
+  })
+)
