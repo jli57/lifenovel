ad367f4c814a3296e9ff2827b9aba992ec2242f0
Jingna Li <jingna13@gmail.com>
2018-10-09 17:56:57 -0700

    Add user output to receiveCurrentUser

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -6,6 +6,7 @@ export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser = (user) => ({
   type: RECEIVE_CURRENT_USER,
+  user
 });
 const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
