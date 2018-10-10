7439623c5aed0a92faf3243f60233f183aade078
Jingna Li <jingna13@gmail.com>
2018-10-10 09:55:01 -0700

    Add create, update, and destroy definitions for posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,32 @@
 class Api::PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render :show
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+    if @post.update(post_params)
+      render :show
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    render json: {}
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author_id, :body, :page_id)
+  end
 end
