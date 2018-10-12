3b274b9bc8746b140af5ce28f24847f0b4bc31d3
Jingna Li <jingna13@gmail.com>
2018-10-11 18:37:20 -0700

    Add post index class

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PostIndexItem from './post_index_item';
 
 const PostIndex = ({ currentUser, posts, updatePost, deletePost }) => (
-  <div>
+  <div className="post-index">
     <h1>Posts</h1>
     <ul className="flex-vert">
       { posts.map( post => (
