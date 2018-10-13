5616401f24ada6818ee92126d68a27e054464775
Jingna Li <jingna13@gmail.com>
2018-10-12 18:07:51 -0700

    Add action and reducers for modals to take arguments

---
+++ frontend/reducers/modal_args_reducer.js
@@ -0,0 +1,17 @@
+import {
+  OPEN_MODAL,
+  CLOSE_MODAL
+} from '../actions/modal_actions';
+
+const modalArgsReducer = ( state = [], action ) => {
+  switch( action.type) {
+    case OPEN_MODAL:
+      return action.modalArgs;
+    case CLOSE_MODAL:
+      return [];
+    default:
+      return state;
+  }
+}
+
+export default modalArgsReducer;
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import modalReducer from './modal_reducer';
+import modalArgsReducer from './modal_args_reducer';
 
 const uiReducer = combineReducers({
-  modal: modalReducer
+  modal: modalReducer,
+  modalArgs: modalArgsReducer
 });
 
 export default uiReducer;
