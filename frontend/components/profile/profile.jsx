3747ac910cb00e967e585923a60d764f50c19d43
Jingna Li <jingna13@gmail.com>
2018-11-09 15:31:47 -0800

    Remove page not found when refreshing webpage on friend's profile

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import FriendIndexContainer from '../friends/friend_index_container';
 import PostIndexContainer from '../posts/post_index_container';
-import AboutContainer from '../about/about_container'; 
+import AboutContainer from '../about/about_container';
 import ProfileHeaderContainer from './profile_header_container';
 import PageNotFound from '../main/page_not_found';
+import { BeatLoader } from 'react-spinners';
 
 class Profile extends React.Component {
 
@@ -12,9 +13,9 @@ class Profile extends React.Component {
 
     if ( window.performance ) {
       if ( performance.navigation.type === 1 ) {
-        // console.log("Page reloaded"); 
-      }  
-    } 
+        // console.log("Page reloaded");
+      }
+    }
 
   }
 
@@ -33,7 +34,9 @@ class Profile extends React.Component {
     const { profileUser, relType } = this.props;
 
     if ( profileUser === undefined ) return (
-      <PageNotFound />
+      <BeatLoader
+      className="loader"
+      color={'#3B5998'}/>
     );
 
     const profileContent = () => {
@@ -47,7 +50,7 @@ class Profile extends React.Component {
             <PostIndexContainer />
           </section>
         </div>
-      ) : null  
+      ) : null
     }
 
     return (
