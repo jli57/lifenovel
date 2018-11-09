282f70f9d13749d52058581927412229f93ce24e
Jingna Li <jingna13@gmail.com>
2018-11-09 15:18:54 -0800

    Fix pixel sizing for search bar

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import HeaderModal from '../modal/header_modal'; 
+import HeaderModal from '../modal/header_modal';
 
 class SearchBar extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      searchText: "", 
-      showModal: false, 
+      searchText: "",
+      showModal: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleOnFocus = this.handleOnFocus.bind(this); 
+    this.handleOnFocus = this.handleOnFocus.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.searchUsers(this.state.searchText); 
+    this.props.searchUsers(this.state.searchText);
   }
 
   update(prop) {
     return (e) => {
       e.preventDefault();
-      this.setState({[prop]: e.target.value}, 
-        () => {this.props.searchUsers(this.state.searchText)}  
+      this.setState({[prop]: e.target.value},
+        () => {this.props.searchUsers(this.state.searchText)}
       );
     }
   }
 
   handleOnFocus(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     // this.setState({showModal: !this.state.showModal})
-    this.props.openModal( "search", {} ); 
+    this.props.openModal( "search", {} );
   }
 
 
